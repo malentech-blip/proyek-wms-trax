7705e681e4a7272dc0f7f9adf98dd1893f7ebd5b
Hendra Adriyanto Permana Putra <hendrapermanadev@gmail.com>
2025-10-18 17:23:59 +0700

    feat(db): create migration for admin production module

---
+++ database/migrations/2025_10_14_152914_create_finished_goods_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('finished_goods', function (Blueprint $table) {
+      $table->id();
+      $table->foreignId('wip_id')->constrained('wip_records');
+      $table->foreignId('item_id')->constrained('items');
+      $table->foreignId('label_id')->constrained('item_labels');
+      $table->unsignedInteger('quantity');
+      $table->string('qc_status');
+      $table->dateTime('stored_at');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('finished_goods');
+  }
+};
