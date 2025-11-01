13c209aae5c39b57242f9a6ede943cd2c435ad97
Neno Arisma <nenoarisma@gmail.com>
2025-11-01 14:42:45 +0700

    feat(models): add relationships for location, rack, and pallet in ItemLabel model

--- app/Models/Admin/Inbound/ItemLabel.php
+++ app/Models/Admin/Inbound/ItemLabel.php
@@ -4,8 +4,24 @@ namespace App\Models\Admin\Inbound;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ItemLabel extends Model
 {
     use HasFactory;
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\SuperAdmin\MasterData\Location::class);
+    }
+
+    public function rack(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\SuperAdmin\MasterData\Rack::class);
+    }
+
+    public function pallet(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\SuperAdmin\MasterData\Pallet::class);
+    }
 }
